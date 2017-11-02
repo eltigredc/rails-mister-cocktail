764e716db8742105f53b7343f586e118e8e24395
Diogo Heinen <eltigredc@gmail.com>
2017-11-02 16:24:47 +0100

    changes

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
     resources :doses, only:[:new, :create, :show]
   end
   resources :doses, only:[:destroy]
-  root :to "cocktails"
+  root "cocktails#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
